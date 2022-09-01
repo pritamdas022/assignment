d5e93995824094263cdc7d939f36eebdefbfb2fa
pritamdas022 <110466018+pritamdas022@users.noreply.github.com>
2022-09-01 23:06:30 +0530

    Add files via upload

---
+++ myProgram.java
@@ -0,0 +1,13 @@
+/* problem  statement:
+ 
+   Fix the compilation errors and run the program?the program should print "My First Java Program"in the console.*/
+package JavaProgram;
+
+public class myProgram {
+
+	public static void main(String[] args) {
+		System.out.println("My First Java Program");
+
+	}
+
+}
